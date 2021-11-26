4bd53a51dd530b0d5c01cb9476c9579e043473df
Istiaq Hossain <istiaq.hossain@ejogajogbd.com>
2021-11-26 22:08:32 +0600

    code refactoring for unit testing

--- src/test/java/com/todotask/api/TodoTaskApplicationTests.java
+++ src/test/java/com/todotask/api/TodoTaskApplicationTests.java
@@ -1,10 +1,10 @@
-package com.geolocation.api;
+package com.todotask.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GeolocationApplicationTests {
+class TodoTaskApplicationTests {
 
 	@Test
 	void contextLoads() {

ba41005a5e8f77d2f986f9713aee7f1e5dbe2762
Istiaq Hossain <istiaq.hossain@ejogajogbd.com>
2021-11-27 11:47:30 +0600

    add unit testing for todotask repository

--- src/test/java/com/todotask/api/TotoTaskRepositryTest.java
+++ src/test/java/com/todotask/api/TotoTaskRepositryTest.java
@@ -2,13 +2,9 @@ package com.todotask.api;
 
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
